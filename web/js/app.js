65bd53a1295aa32875ac62fcc73452598c43ffc7
Pablo Felipe <pablofelipe@gmail.com>
2026-01-18 23:06:47 -0300

    Add web folder

---
+++ web/js/app.js
@@ -0,0 +1,6 @@
+console.log("DealApp carregado");
+
+// Placeholder para login Google no futuro
+document.getElementById("loginBtn")?.addEventListener("click", () => {
+    alert("Login com Google será implementado aqui");
+});
